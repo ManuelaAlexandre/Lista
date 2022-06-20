affc230be5c3a77318872f7c06a37d39b08c260d
ManuelaAlexandre <107817760+ManuelaAlexandre@users.noreply.github.com>
2022-06-20 12:16:13 -0300

    Add files via upload

---
+++ exer1.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+public class exer1 {
+    public static void main(String[] args) {
+
+        Scanner input = new Scanner(System.in);
+       
+        Double B,b,h,A;
+       
+        System.out.println("Digite a base maior: ");
+        B = input.nextDouble();
+       
+        System.out.println("Digite a base menor: ");
+        b = input.nextDouble();
+       
+        System.out.println("Digite a  altura");
+        h = input.nextDouble();
+       
+        A = h*((B + b)/2);
+       
+        System.out.println("A área do trapezio é: " +A);
+    
+    }
+}
