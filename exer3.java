fea0c420ffb8de7a5826f008b47dca57886fd68c
ManuelaAlexandre <107817760+ManuelaAlexandre@users.noreply.github.com>
2022-06-20 12:20:17 -0300

    Add files via upload

---
+++ exer3.java
@@ -0,0 +1,17 @@
+public class exer3 {
+    public static void main(String[] args) {
+        
+        Integer a,b,c,d;
+
+        a = 3-2-1+2+1+3;
+        b = (2 * 3)-(4 * 5);
+        c = 2 + 6 - (( 3 / 7)  * 9);
+        d = 3 % 4 - 8;
+       
+        System.out.println("O resultado do a é: " +a);
+        System.out.println("O resultado do b é:" +b);
+        System.out.println("O resultado do c é: " +c);
+        System.out.println("O resultado do d é: " +d);
+        }
+    }
+
